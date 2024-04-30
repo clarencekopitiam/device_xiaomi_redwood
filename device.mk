9aef1a2324a7997ce1f1856145897482242300b0
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-05-01 00:53:18 +0600

    redwood: PixelOS

--- device.mk
+++ device.mk
@@ -13,8 +13,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Overlays-RRO
 PRODUCT_PACKAGES += \
